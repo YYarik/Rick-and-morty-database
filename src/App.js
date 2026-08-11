8a4b18b7d94c8e209a90b69c345ade8aa7e97034
Yaroslav Yashchuk <gaubetsa@gmail.com>
2026-08-11 18:08:32 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 import Pagination from './components/Pagination/Pagination';
 import Search from './components/Search/Search';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Episodes from './Pages/Episodes';
+import Location from './Pages/Location';
+
+function App(){
+  return(
+    <Router>
+      <div className="App">
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/episodes' element={<Episodes/>}/>
+        <Route path='/location' element={<Location/>}/>
+      </Routes>
+    </Router>
+  )
+}
+const Home = () => {
 
   let [pageNumber, setPageNumber] = useState(1);
   let [search, setSearch] = useState("");
@@ -27,9 +46,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-center ubuntu my-4">
-        Rick & Morty <span className="text-primary">WiKi</span>
-      </h1>
+
+
 
       <Search 
         setPageNumber={setPageNumber}
